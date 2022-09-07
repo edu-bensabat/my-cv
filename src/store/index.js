7deb74877239a859c2d53c06255402fd1d0273d8
jvacdragon <joao_aguiarcosta@hotmail.com>
2022-09-06 22:28:33 -0300

    removing componentConfig

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { componentConfigReducer } from "./ComponentConfig";
 import { modeReducer } from "./mode";
 
 const store = configureStore({
-  reducer: { componentConfig: componentConfigReducer, mode: modeReducer },
+  reducer: { mode: modeReducer },
 });
 
 export default store;
